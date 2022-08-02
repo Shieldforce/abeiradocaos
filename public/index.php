417e0bba7ee82fce60a409c4b80eb133e513d7ae
Alexandre Ferreira <shieldforce2@gmail.com>
2022-08-02 12:15:19 -0300

    Criando classe Request

--- public/index.php
+++ public/index.php
@@ -1,13 +1,21 @@
 <?php
 
-use App\BootSystem;
+use Dotenv\Dotenv;
+use Core\BootSystem;
 
 require(__DIR__."/../vendor/autoload.php");
 
 try {
+
+    $dotenv = Dotenv::createMutable(__DIR__."/../");
+    $dotenv->load();
+
     BootSystem::execute();
+
 } catch(\Exception $exception) {
+
     echo "<pre>";
     print_r($exception);
     echo "</pre>";
+
 }
