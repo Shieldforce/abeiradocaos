8174d7a0fae7892934c6674b3fc3e8ddacac3b98
Alexandre Ferreira <shieldforce2@gmail.com>
2022-08-02 12:05:45 -0300

    Instalando variaveis de ambiente e Classe de Boot do projeto!

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
 <?php
 
+use App\BootSystem;
+
 require(__DIR__."/../vendor/autoload.php");
+
+try {
+    BootSystem::execute();
+} catch(\Exception $exception) {
+    echo "<pre>";
+    print_r($exception);
+    echo "</pre>";
+}
